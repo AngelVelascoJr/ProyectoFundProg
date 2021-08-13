3952e2661a3570d1cd26f33e2b276f498b09799a
AngelVelascoJr <80130331+AngelVelascoJr@users.noreply.github.com>
2021-08-13 02:04:14 -0500

    Update Test1.cpp

--- Test1.cpp
+++ Test1.cpp
@@ -291,6 +291,7 @@ void Stadistics()
 	printf("8: %i\n", Users[UserIndex].CalifPorJuego[3]);
 	printf("9: %i\n", Users[UserIndex].CalifPorJuego[4]);
 	printf("10: %i\n", Users[UserIndex].CalifPorJuego[5]);
+	
 }
 
 void DataFromFile()	//carga el archivo de perfiles
